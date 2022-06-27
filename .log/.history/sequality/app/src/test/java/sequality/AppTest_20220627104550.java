1d91b957fe822d039b88db53f8ca79718bf89a59
HidekiSuruga <maruwa512@gmail.com>
2022-06-27 10:47:32 +0900

    hellow

--- .log/.history/sequality/app/src/test/java/sequality/AppTest_20220627104550.java
+++ .log/.history/sequality/app/src/test/java/sequality/AppTest_20220627104550.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-  @Test
+    @Test
   public void testAppReternHelloWorld() {
     App classUnderTest = new App();
     assertEquals("Hello World!", classUnderTest.getGreeting());
